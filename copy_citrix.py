eaba9c712054c15f2579ae25a0f20d862c37523f
Kurtis Hanson <kxrtiswithak@gmail.com>
2026-02-18 01:19:13 +0000

    handle new lines

--- copy_citrix.py
+++ copy_citrix.py
@@ -7,6 +7,12 @@ clipboard = clipboard.paste() # Get clipboard information
 
 time.sleep(5) # Wait 5 seconds
 for letter in str(clipboard):
-    keyboard.press(letter)
-    keyboard.release(letter)
+    if letter == '\r':
+        continue  # Skip carriage return, handle newline with \n
+    elif letter == '\n':
+        keyboard.press(Key.enter)
+        keyboard.release(Key.enter)
+    else:
+        keyboard.press(letter)
+        keyboard.release(letter)
     time.sleep(0.005) # Wait some miliseconds between key presses for better performance
